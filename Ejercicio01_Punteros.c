6119dd6a5e3b654550bece451943ef8dc656fd8f
majo <mjmanzo@espe.edu.ec>
2026-06-29 19:30:42 -0500

    Ejercicio 1
    Implement a program that calculates squares and cubes of numbers up to a user-defined limit using pointers.

---
+++ Ejercicio01_Punteros.c
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int tope;
+    int *pTope = &tope;
+
+    printf("Ingrese el numero tope: ");
+    scanf("%d", pTope);
+
+    printf("\n");
+    printf("%-10s %-12s %-10s\n", "Numero", "Cuadrado", "Cubo");
+    printf("--------------------------------------\n");
+
+    int i;
+    int *pI = &i;
+
+    for (*pI = 1; *pI <= *pTope; (*pI)++) {
+
+        printf("%-10d %-12d %-10d\n",
+               *pI,
+               (*pI) * (*pI),
+               (*pI) * (*pI) * (*pI));
+    }
+
+    return 0;
+}
